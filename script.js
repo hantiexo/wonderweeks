1ede12bb87fd151a453adfb28b03143d9623dff7
hantiexo <72255404+hantiexo@users.noreply.github.com>
2024-11-07 20:38:34 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function startTracking() {
+    alert("Welcome! Please enter your baby’s birth date to start tracking their development.");
+}
+
+// Contact form submission (for demonstration purposes)
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for contacting us! We will get back to you soon.');
+});
